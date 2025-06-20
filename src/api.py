e3a2fff77cad9f061bd58a3148bdbfc552b4c939
Gabriel Leal <marqsleal@gmail.com>
2025-06-19 23:51:59 -0300

    Reconfigurando cors

--- src/api.py
+++ src/api.py
@@ -21,10 +21,14 @@ app = FastAPI(title="Warhammer 40k Damage Calculator API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080"],
+    allow_origins=[
+        "http://localhost:8080",
+        "http://127.0.0.1:8080",
+        "http://0.0.0.0:8080"
+    ],
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["POST"],
+    allow_headers=["Content-Type"],
 )
 
 class AttackerModel(BaseModel):
